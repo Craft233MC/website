d6b2d8bda531890b05fb96a07c80cf7893eb37a5
neokoni <dulin081682@126.com>
2024-08-05 02:02:52 +0800

    meta标签增加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,83 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        {
+          name: "description",
+          content: "Craft233，一个休闲养老摸鱼系的Minecraft小服务器，欢迎加入我们，同我们一起成长！",
+        },
+        {
+          name: "robots",
+          content: "index, follow, max-image-preview:large, max-snippet:-1",
+        },
+        {
+          property: "og:locale",
+          content: "zh_CN",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:title",
+          content: "Craft233 ｜ 主页",
+        },
+        {
+          property: "og:url",
+          content: "https://www.craft233.top",
+        },
+        {
+          property: "og:site_name",
+          content: "Craft233 ｜ 主页",
+        },
+        {
+          property: "og:image",
+          content: "https://www.craft233.top/assets/img/index/town.png",
+        },
+        {
+          property: "og:image:width",
+          content: "1920",
+        },
+        {
+          property: "og:width:height",
+          content: "1080",
+        },
+        {
+          property: "og:image:type",
+          content: "image/png",
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          name: "twitter:title",
+          content: "Craft233 ｜ 主页",
+        },
+        {
+          property: "twitter:description",
+          content: "Craft233，一个休闲养老摸鱼系的Minecraft小服务器，欢迎加入我们，同我们一起成长！",
+        },
+        {
+          itemprop: "name",
+          content: "Craft233 | 主页",
+        },
+        {
+          itemprop: "description",
+          content: "Craft233，一个休闲养老摸鱼系的Minecraft小服务器，欢迎加入我们，同我们一起成长！",
+        },
+        {
+          itemprop: "image",
+          content: "https://resource.neokoni.moe/craft233/img/icon_main.png",
+        },
+        {
+          itemprop: "description",
+          content: "Craft233，一个休闲养老摸鱼系的Minecraft小服务器，欢迎加入我们，同我们一起成长！",
+        },
+      ],
+      // script
       script: [{
         src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.9/pdfmake.min.js",
         defer: true
