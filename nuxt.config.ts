7f2ca84fa6212aec6d9575f92d5991227966720d
neokoni <dulin081682@126.com>
2025-03-11 01:57:06 +0800

    fix: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,6 +51,10 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [
+                { rel: "icon", type: "image/x-icon", href: "favicon.ico" }
+      ],
+
       meta: [
         {
           name: "description",
